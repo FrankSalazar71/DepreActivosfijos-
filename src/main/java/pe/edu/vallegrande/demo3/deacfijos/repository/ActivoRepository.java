111dc85e4dd40e49bb8a841705b00ed80af10c9b
FaviohuamanVG <favio.huaman@vallegrande.edu.pe>
2025-05-17 11:27:54 -0500

    Deleted files

--- src/main/java/pe/edu/vallegrande/demo3/deacfijos/repository/ActivoRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.vallegrande.demo3.deacfijos.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pe.edu.vallegrande.demo3.deacfijos.model.Activo;
-
-public interface ActivoRepository extends MongoRepository<Activo, String> {
-}
